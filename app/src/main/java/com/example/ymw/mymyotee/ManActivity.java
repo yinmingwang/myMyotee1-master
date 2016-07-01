cc1ac222fbe2de4009c49119f5ed1b2f4bf159ac
yinmingwang <972576849@qq.com>
2016-07-01 09:46:48 +0800

    final
    the final

--- app/src/main/java/com/example/ymw/mymyotee/ManActivity.java
+++ app/src/main/java/com/example/ymw/mymyotee/ManActivity.java
@@ -12,6 +12,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.shizhefei.view.indicator.IndicatorViewPager;
@@ -31,6 +34,33 @@ public class ManActivity extends FragmentActivity {
             R.drawable.face_selector,
             R.drawable.eye_selector,
     };
+    private int[] eyes = new int[] {
+            R.drawable.eye_0,R.drawable.eye_1,R.drawable.eye_2,R.drawable.eye_3,R.drawable.eye_4, R.drawable.eye_5,R.drawable.eye_6,R.drawable.eye_7,R.drawable.eye_8,
+            R.drawable.eye_9,R.drawable.eye_10,R.drawable.eye_11, R.drawable.eye_12,R.drawable.eye_13, R.drawable.eye_14,R.drawable.eye_15,R.drawable.eye_16,
+            R.drawable.eye_17,R.drawable.eye_18,R.drawable.eye_19,R.drawable.eye_20, R.drawable.eye_21,R.drawable.eye_22,R.drawable.eye_23,R.drawable.eye_24,
+            R.drawable.eye_25, R.drawable.eye_26,R.drawable.eye_27,R.drawable.eye_28,R.drawable.eye_29, R.drawable.eye_30,R.drawable.eye_31,R.drawable.eye_32,
+            R.drawable.eye_33,R.drawable.eye_34,R.drawable.eye_35,R.drawable.eye_36,R.drawable.eye_37,R.drawable.eye_38, R.drawable.eye_39,R.drawable.eye_40,
+            R.drawable.eye_41,R.drawable.eye_42,R.drawable.eye_43,R.drawable.eye_44,R.drawable.eye_45,R.drawable.eye_46,R.drawable.eye_47,R.drawable.eye_48,
+            R.drawable.eye_49,R.drawable.eye_50,R.drawable.eye_51,R.drawable.eye_52
+    };
+    private int[] hair = new int[] {
+            R.drawable.hair_0,R.drawable.hair_1,R.drawable.hair_2,R.drawable.hair_3,R.drawable.hair_4,R.drawable.hair_5,R.drawable.hair_6,R.drawable.hair_7,
+            R.drawable.hair_8,R.drawable.hair_9,R.drawable.hair_10,R.drawable.hair_11,R.drawable.hair_12,R.drawable.hair_13,R.drawable.hair_14,R.drawable.hair_15,
+            R.drawable.hair_16,R.drawable.hair_17,R.drawable.hair_18,R.drawable.hair_19,R.drawable.hair_20,R.drawable.hair_21,R.drawable.hair_22,R.drawable.hair_23,
+            R.drawable.hair_24,R.drawable.hair_25,R.drawable.hair_26,R.drawable.hair_27,R.drawable.hair_28,R.drawable.hair_29,R.drawable.hair_30,R.drawable.hair_31,
+            R.drawable.hair_32,R.drawable.hair_33,R.drawable.hair_34,R.drawable.hair_35,R.drawable.hair_36,R.drawable.hair_37,R.drawable.hair_38,R.drawable.hair_39,
+            R.drawable.hair_40,R.drawable.hair_41,R.drawable.hair_42,R.drawable.hair_43,R.drawable.hair_44,R.drawable.hair_45,R.drawable.hair_46,R.drawable.hair_47,
+            R.drawable.hair_48,R.drawable.hair_49,R.drawable.hair_50,R.drawable.hair_51,R.drawable.hair_52,R.drawable.hair_53,R.drawable.hair_54,R.drawable.hair_55,
+            R.drawable.hair_56,R.drawable.hair_57,R.drawable.hair_58,R.drawable.hair_59,R.drawable.hair_60,R.drawable.hair_61,R.drawable.hair_62,R.drawable.hair_63,
+            R.drawable.hair_64,R.drawable.hair_65,R.drawable.hair_66,R.drawable.hair_67,R.drawable.hair_68,R.drawable.hair_69,R.drawable.hair_70,R.drawable.hair_71,
+            R.drawable.hair_72,R.drawable.hair_73,R.drawable.hair_74
+    };
+    private int[] faces = new int[] {
+            R.drawable.face_20000,R.drawable.face_20001,R.drawable.face_20002,R.drawable.face_20003,R.drawable.face_20004,R.drawable.face_20005,
+            R.drawable.face_20006,R.drawable.face_20007,R.drawable.face_20008,R.drawable.face_20009,R.drawable.face_20010,R.drawable.face_20011,
+            R.drawable.face_20012,R.drawable.face_20013,R.drawable.face_20014,R.drawable.face_20015,R.drawable.face_20016,R.drawable.face_20017,
+            R.drawable.face_20018,R.drawable.face_20019
+    };
     private ScrollIndicatorView scrollIndicatorView;
     private ToggleButton pinnedToggleButton;
     private ToggleButton splitAutotoggleButton;
@@ -40,6 +70,32 @@ public class ManActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.man);
+        final ImageButton manimagebutton1 = (ImageButton) findViewById(R.id.manimageButton1);
+        manimagebutton1.setImageResource(R.drawable.img_return);
+        manimagebutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        final ImageButton manimagebutton2 = (ImageButton) findViewById(R.id.manimageButton2);
+        manimagebutton2.setImageResource(R.drawable.bt_save_up);
+        manimagebutton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ManActivity.this, "已保存", Toast.LENGTH_SHORT).show();
+            }
+        });
+        final ImageButton manimagebuton3 = (ImageButton) findViewById(R.id.manimageButton3);
+        manimagebuton3.setImageResource(R.drawable.bt_share_up);
+        manimagebuton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ManActivity.this, "已分享", Toast.LENGTH_SHORT).show();
+            }
+        });
+        final ImageView manimageview1 = (ImageView) findViewById(R.id.manimageView1);
+        manimageview1.setImageResource(R.drawable.demo_pic);
         ViewPager viewPager = (ViewPager) findViewById(R.id.moretab_viewPager);
         scrollIndicatorView = (ScrollIndicatorView) findViewById(R.id.moretab_indicator);
         scrollIndicatorView.setBackgroundColor(Color.WHITE);
